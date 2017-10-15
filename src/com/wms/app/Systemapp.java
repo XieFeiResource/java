a7ac3b69309255fa053968158362af559059fa65
Administrator <Administrator@SC-201709302348>
2017-10-15 18:37:00 +0800

    twice modify

--- src/com/wms/app/Systemapp.java
+++ src/com/wms/app/Systemapp.java
@@ -2,10 +2,11 @@ package com.wms.app;
 
 import java.util.Scanner;
 
-public class System {
-	public static int count=0;
+public class Systemapp {
+	public static int count=0;	
 	public static int n=7;
 	public static String [][]good=new String[50][3];//商品名称，价格，剩余量
+	public static String [][]buycar=new String[50][3];
 	static {
 		good[0][0]="oppo";	good[0][1]="5000";	good[0][2]="0001";
 		good[1][0]="opppoo"; good[1][1]="5050"; good[1][2]="0002";
@@ -16,45 +17,47 @@ public class System {
 		good[6][0]="vivo"; good[6][1]="2005"; good[6][2]="0007";
 	}
 	public static void mainMenu() {
-	java.lang.System.out.println("*******欢迎来到仓库管理系统*******");
-	java.lang.System.out.println("**********1.录入商品**********");
-	java.lang.System.out.println("**********2.展示所有商品**********");
-	java.lang.System.out.println("**********3.删除商品*******");
-	java.lang.System.out.println("**********4.修改商品**********");
-	java.lang.System.out.println("**********5.模糊搜索**********");
-	java.lang.System.out.println("**********7.退出系统**********");
-	java.lang.System.out.print("请按1~7进行相关操作");
+	System.out.println("*******欢迎来到超市管理系统*******");
+	System.out.println("**********1.录入商品**********");
+	System.out.println("**********2.展示所有商品**********");
+	System.out.println("**********3.删除商品*******");
+	System.out.println("**********4.修改商品**********");
+	System.out.println("**********5.模糊搜索**********");
+	System.out.println("**********6.购买商品**********");
+	System.out.println("**********7.结账**********");
+	System.out.println("**********8.退出系统**********");
+	System.out.print("请按1~7进行相关操作");
 	}
 	public static void modifyMenu() {
-		java.lang.System.out.println("******修改界面*******");
-		java.lang.System.out.println("******1.修改价格 *******");
-		java.lang.System.out.println("******2.修改编号 *******");
-		java.lang.System.out.println("请按1~2进行相关操作");
+		System.out.println("******修改界面*******");
+		System.out.println("******1.修改价格 *******");
+		System.out.println("******2.修改编号 *******");
+		System.out.println("请按1~2进行相关操作");
 	}
 	public static void loginMenu() {
-		java.lang.System.out.println("******隆中超市管理系统*******");
-		java.lang.System.out.println("******请先登录*******");	
+		System.out.println("******隆中超市管理系统*******");
+		System.out.println("******请先登录*******");	
 		login();
 	}
 	public static void login(){
-		java.lang.System.out.println("请输入用户名：");
+		System.out.println("请输入用户名：");
 		Scanner in=new Scanner(java.lang.System.in);
 		String username=in.next();
-		java.lang.System.out.println("请输入密码：");
+		System.out.println("请输入密码：");
 		String  password=in.next();
 //		in.close();
 		if(username.equals("Tom")&&password.equals("123"))
 		{
-			java.lang.System.out.println("登陆成功！");
+			System.out.println("登陆成功！");
 			mainMenu();
 		}
 		else{			
 			if(count >=1){
-				java.lang.System.out.println("对不起！亲，您已经没有机会了！");
+				System.out.println("对不起！亲，您已经没有机会了！");
 				return;
 			}else{
 				count++;
-				java.lang.System.out.println("对不起，你输入的用户名或密码错误！请重新输入！");
+				System.out.println("对不起，你输入的用户名或密码错误！请重新输入！");
 				login();
 			}			
 		}
@@ -62,19 +65,19 @@ public class System {
 	public static void addGood() {//添加商品
 		String judge;
 		do {
-		Scanner in=new Scanner(java.lang.System.in);
-		java.lang.System.out.println("请输入添加的商品名称");
+		Scanner in=new Scanner(System.in);
+		System.out.println("请输入添加的商品名称");
 		String name=in.next();
-		java.lang.System.out.println("请输入添加的商品价格");
+		System.out.println("请输入添加的商品价格");
 		String price=in.next();
-		java.lang.System.out.println("请输入添加的商品编码");
+		System.out.println("请输入添加的商品编码");
 		String amount=in.next();
 //		in.close();
 		good[n][0]=name;
 		good[n][1]=price;
 		good[n][2]=amount;
 		n++;
-		java.lang.System.out.println("是否继续添加？输入Y/y或N/n");
+		System.out.println("是否继续添加？输入Y/y或N/n");
 		judge=in.next();
 		}while(judge.equalsIgnoreCase("y"));
 	}
@@ -87,15 +90,15 @@ public class System {
 			if(n % pagesize != 0){
 				totalPageNumber++;
 			}
-			java.lang.System.out.println("*****超市仓库所有商品******");
+			System.out.println("*****超市仓库所有商品******");
 			do {
-			java.lang.System.out.println("*****第"+page+"页******");
-			java.lang.System.out.println("名称\t 价格\t 编码\t");
+			System.out.println("*****第"+page+"页******");
+			System.out.println("名称\t 价格\t 编码\t");
 			for(int k=i;k<n;k++) {
 				for(int j=0;j<3;j++) {
-					java.lang.System.out.print(good[k][j]+"\t");
+					System.out.print(good[k][j]+"\t");
 				}
-				java.lang.System.out.println();	
+				System.out.println();	
 				if((k+1)%pagesize==0) {
 					i=k+1;
 					break;
@@ -172,9 +175,42 @@ public class System {
 			}	
 		}
 	}
+	public static void buyGoods() {
+		showGoods();
+		String judge;
+		do {
+		Scanner in=new Scanner(java.lang.System.in);
+		java.lang.System.out.println("请输入想购买的商品名称");
+		String goodname=in.next();
+		int k=searchGood(goodname);
+		if(k<0) {
+			System.out.println("没有该商品");
+		}else {
+		java.lang.System.arraycopy(good[k], 0, buycar[count], 0, 3);
+		count++;
+		}
+		java.lang.System.out.println("是否继续购买？输入Y/y或N/n");
+		judge=in.next();		
+	  }while(judge.equalsIgnoreCase("y"));
+		java.lang.System.out.println("您购买的商品详情如下");
+		java.lang.System.out.println("名称\t 价格\t 编码\t");
+		for(int i=0;i<count;i++) {
+			for(int j=0;j<3;j++) {
+				java.lang.System.out.print(buycar[i][j]+"\t");
+			}
+			java.lang.System.out.println();
+		}
+	}
+	public static void account() {
+		int sum=0;
+		for(int i=0;i<count;i++) {
+			sum=sum+Integer.parseInt(buycar[i][1]);
+		}
+		java.lang.System.out.println("您一共需要支付"+sum+"$");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int quit=7;
+		int quit=8;
 		loginMenu();
 		if(count >=1) java.lang.System.exit(0);
 		while(true) {
@@ -203,9 +239,13 @@ public class System {
 			case 4:{
 				java.lang.System.out.println("请输入要修改商品的名称");
 				String name1=in.next();
+				int i=searchGood(name1);
+				if(i<0) {
+					System.out.println("没有该商品");
+				}else {
 				modifyMenu();
 				modifyGood(name1);
-				java.lang.System.out.println("修改成功");
+				}
 				mainMenu();
 				break;
 			}
@@ -213,6 +253,16 @@ public class System {
 				java.lang.System.out.println("请输入要搜索的商品的名称");
 				String name1=in.next();
 				moHuSearch(name1);
+				mainMenu();
+				break;
+			}
+			case 6:{
+				buyGoods();
+				mainMenu();
+				break;
+			}
+			case 7:{
+				account();
 				mainMenu();
 				break;
 			}
